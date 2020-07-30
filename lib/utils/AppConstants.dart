be532c45beb69e3a476eb45099e07c6fcef954ad
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2020-07-30 12:28:50 -0700

    upload 1.2 linux

--- lib/utils/AppConstants.dart
+++ lib/utils/AppConstants.dart
@@ -4,19 +4,39 @@ class AppConstants {
   static const String AppName = "NavokiNotes";
 
   // TODO: Replace your key here
-  static const String API_KEY = "";
+  static const String API_KEY = "AIzaSyCIJoxJ6o-KMhxXlQIcLAdtz5J5WVKFSDI";
 
   static material.Color orangeColor = const material.Color(0xffff470b);
   static material.Color yellowColor = const material.Color(0xfffca10e);
 
   static List socialLinks = [
-    {'name': 'youtube', "link": 'https://www.youtube.com/channel/UCP2-MYtIbBnlEcfTvJKo5Og?sub_confirmation=1', 'icon': 'assets/images/youtube.png'},
-    {'name': 'github', "link": 'https://www.github.com/theshivamlko', 'icon': 'assets/images/github.png'},
-    {'name': 'linkedin', "link": 'https://www.linkedin.com/in/theshivamlko', 'icon': 'assets/images/linkedin.png'},
-    {'name': 'twitter', "link": 'https://twitter.com/theshivamlko', 'icon': 'assets/images/twitter.png'},
-    {'name': 'website', "link": 'https://navoki.com', 'icon': 'assets/images/website.png'},
+    {
+      'name': 'youtube',
+      "link":
+          'https://www.youtube.com/channel/UCP2-MYtIbBnlEcfTvJKo5Og?sub_confirmation=1',
+      'icon': 'assets/images/youtube.png'
+    },
+    {
+      'name': 'github',
+      "link": 'https://www.github.com/theshivamlko',
+      'icon': 'assets/images/github.png'
+    },
+    {
+      'name': 'linkedin',
+      "link": 'https://www.linkedin.com/in/theshivamlko',
+      'icon': 'assets/images/linkedin.png'
+    },
+    {
+      'name': 'twitter',
+      "link": 'https://twitter.com/theshivamlko',
+      'icon': 'assets/images/twitter.png'
+    },
+    {
+      'name': 'website',
+      "link": 'https://navoki.com',
+      'icon': 'assets/images/website.png'
+    },
   ];
-
 
 // Keys Constants
   static const EMAIL = "email";
@@ -24,7 +44,4 @@ class AppConstants {
   static const API_TOKEN = "token";
   static const LOCAL_ID = "localId";
   static const REQUEST_TYPE = "requestType";
-
 }
-
-
