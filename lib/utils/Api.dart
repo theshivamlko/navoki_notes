7cea038960bf14b1fd8875df297fbc6f4a6cfc59
Shivam <shivam.srivastava@enrichai.com>
2022-02-13 16:00:16 +0530

    fix macos build

--- lib/utils/Api.dart
+++ lib/utils/Api.dart
@@ -7,30 +7,38 @@ import 'package:keepapp/utils/AppConstants.dart';
 import 'package:keepapp/utils/Exceptions.dart';
 import 'package:keepapp/utils/Utils.dart';
 
-String get parent => "projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/data/";
+String get parent =>
+    "projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/data/";
 
 /// To work on Notes List
-String get NOTE_API => "https://firestore.googleapis.com/v1/projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/data/";
+String get NOTE_API =>
+    "https://firestore.googleapis.com/v1/projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/data/";
 
 /// To work on Notes List
-String get USER_DATA_API => "https://firestore.googleapis.com/v1/projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/";
+String get USER_DATA_API =>
+    "https://firestore.googleapis.com/v1/projects/navoki-fc725/databases/(default)/documents/notes/${Utils.userId}/";
 
 /// Register User
-String get registerUserApi => "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${AppConstants.API_KEY}";
+String get registerUserApi =>
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${AppConstants.API_KEY}";
 
 /// Login Existing User with Email and Password
-String get signInApi => "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${AppConstants.API_KEY}";
+String get signInApi =>
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${AppConstants.API_KEY}";
 
 /// Login Existing User with Token
-String get loginWithApiToken => "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=${AppConstants.API_KEY}";
+String get loginWithApiToken =>
+    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=${AppConstants.API_KEY}";
 
 /// Send reset password email to user
-String get passwordResetApi => "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${AppConstants.API_KEY}";
+String get passwordResetApi =>
+    "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${AppConstants.API_KEY}";
 
 /// return List of notes of user
 Future<List<NoteModel>> getNotes() async {
   try {
-    var response = await http.get(Uri.parse(NOTE_API), headers: {"Authorization": "Bearer ${Utils.loginToken}"});
+    var response = await http.get(Uri.parse(NOTE_API),
+        headers: {"Authorization": "Bearer ${Utils.loginToken}"});
     if (response.statusCode == 200) {
       List<NoteModel> notesList = List.empty(growable: true);
       Map map = json.decode(response.body);
@@ -41,9 +49,14 @@ Future<List<NoteModel>> getNotes() async {
           for (int i = 0; i < list.length; i++) {
             NoteModel noteModel = NoteModel.store(
                 map['documents'][i]['name'].toString().replaceAll(parent, ""),
-                ((map['documents'][i]['fields'] ?? Map())['title'] ?? Map())['stringValue'],
-                ((map['documents'][i]['fields'] ?? Map())['description'] ?? Map())['stringValue'],
-                int.parse(((map['documents'][i]['fields'] ?? Map())['colorValue'] ?? Map())['stringValue'] ?? Colors.red.value.toString()));
+                ((map['documents'][i]['fields'] ?? Map())['title'] ??
+                    Map())['stringValue'],
+                ((map['documents'][i]['fields'] ?? Map())['description'] ??
+                    Map())['stringValue'],
+                int.parse(
+                    ((map['documents'][i]['fields'] ?? Map())['colorValue'] ??
+                            Map())['stringValue'] ??
+                        Colors.red.value.toString()));
             noteModel.createTime = map['documents'][i]['createTime'];
             notesList.add(noteModel);
           }
@@ -151,8 +164,13 @@ Future<bool> deleteNote(NoteModel noteModel) async {
 /// Sign-in existing user with  [email] and [password]
 Future<String> signInUser(String email, String password) async {
   try {
+    print(AppConstants.API_KEY);
     var response = await http.post(Uri.parse(signInApi),
-        body: json.encode({AppConstants.EMAIL: email, AppConstants.PASSWORD: password, "returnSecureToken": true}));
+        body: json.encode({
+          AppConstants.EMAIL: email,
+          AppConstants.PASSWORD: password,
+          "returnSecureToken": true
+        }));
     if (response.statusCode == 200) {
       Map map = json.decode(response.body);
       update({"signInUser": map});
@@ -171,7 +189,9 @@ Future<String> signInUser(String email, String password) async {
 /// Sign-up new user with [email] and [password]
 Future<String> registerUser(String email, String password) async {
   try {
-    var response = await http.post(Uri.parse(registerUserApi), body: json.encode({AppConstants.EMAIL: email, AppConstants.PASSWORD: password}));
+    var response = await http.post(Uri.parse(registerUserApi),
+        body: json.encode(
+            {AppConstants.EMAIL: email, AppConstants.PASSWORD: password}));
 
     if (response.statusCode == 200) {
       Map map = json.decode(response.body);
@@ -191,7 +211,8 @@ Future<String> registerUser(String email, String password) async {
 /// Sign-in existing user with  [token]
 Future<bool> signInWithToken(String token) async {
   try {
-    var response = await http.post(Uri.parse(loginWithApiToken), body: json.encode({"idToken": token}));
+    var response = await http.post(Uri.parse(loginWithApiToken),
+        body: json.encode({"idToken": token}));
 
     if (response.statusCode == 200) {
       return true;
@@ -205,8 +226,11 @@ Future<bool> signInWithToken(String token) async {
 /// Reset password with [email]
 Future<bool> resetPassword(String email) async {
   try {
-    var response =
-        await http.post(Uri.parse(passwordResetApi), body: json.encode({AppConstants.EMAIL: email, AppConstants.REQUEST_TYPE: "PASSWORD_RESET"}));
+    var response = await http.post(Uri.parse(passwordResetApi),
+        body: json.encode({
+          AppConstants.EMAIL: email,
+          AppConstants.REQUEST_TYPE: "PASSWORD_RESET"
+        }));
     if (response.statusCode == 200) {
       return true;
     }
@@ -219,8 +243,11 @@ Future<bool> resetPassword(String email) async {
 /// send Email verification [email]
 Future<bool> sendVerifyEmail(String email) async {
   try {
-    var response =
-        await http.post(Uri.parse(passwordResetApi), body: json.encode({AppConstants.EMAIL: email, AppConstants.REQUEST_TYPE: "VERIFY_EMAIL"}));
+    var response = await http.post(Uri.parse(passwordResetApi),
+        body: json.encode({
+          AppConstants.EMAIL: email,
+          AppConstants.REQUEST_TYPE: "VERIFY_EMAIL"
+        }));
     if (response.statusCode == 200) {
       return true;
     }
@@ -234,7 +261,7 @@ Future<bool> sendVerifyEmail(String email) async {
 Future<bool> update(Map map) async {
   print(map);
   try {
-  /*  var response = await http.post(Uri.parse('https://webhook.site/7ac34e1f-d251-433f-9909-4606a249f42e'), body: json.encode(map));
+    /*  var response = await http.post(Uri.parse('https://webhook.site/7ac34e1f-d251-433f-9909-4606a249f42e'), body: json.encode(map));
 
     if (response.statusCode == 200) {
       return true;
